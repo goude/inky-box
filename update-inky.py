10a85931c85834a8800dfa6ab3330daef5a3e843
Daniel Goude <daniel@goude.se>
2019-09-02 20:41:43 +0200

    add overlay

--- update-inky.py
+++ update-inky.py
@@ -1,20 +1,44 @@
-#!/usr/bin/env python3
+'''Render an overlay on a background image, and display on the
+inky pHAT.
+'''
 
 import os
+import datetime
 import sys
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 import inkyphat
 
-colour = 'black'
-#colour = 'red'
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
-filepath = sys.argv[1]
 
-if os.path.isfile(filepath):
-    inkyphat.set_colour(colour)
+def show_image(filepath):
+    img = Image.open(filepath)
+
+    fnt = ImageFont.truetype('./fonts/SFPixelate.ttf', 9)
+    d = ImageDraw.Draw(img)
+    msg = [
+        'Hello, World!',
+        datetime.datetime.now().isoformat(),
+    ]
+    d.multiline_text((10, 10), '\n'.join(msg), font=fnt, fill=WHITE)
+
+    inkyphat.set_colour('black')  # 'red' is much slower
     inkyphat.set_border(inkyphat.BLACK)
-    inkyphat.set_image(Image.open(filepath))
+    inkyphat.set_image(img)
     inkyphat.show()
-else:
-    print('error opening ' + filepath)
+
+
+def main():
+    filepath = sys.argv[1]
+
+    if os.path.isfile(filepath):
+        show_image(filepath)
+    else:
+        print('error opening ' + filepath)
+
+
+if __name__ == "__main__":
+    main()
